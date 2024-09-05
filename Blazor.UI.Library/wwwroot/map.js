b9c672d5a2927bd637e6a62fdc3e91f9ba320039
Michele Tancorre <micheletancorre@gmail.com>
2024-09-05 12:58:06 +0200

    Inserita mappa

---
+++ Blazor.UI.Library/wwwroot/map.js
@@ -0,0 +1,7 @@
+﻿window.showMap = function (latitudine, longitudine, zoom) {
+    var map = L.map('map').setView([latitudine, longitudine], zoom);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
